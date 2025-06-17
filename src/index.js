ad3aa56fee86084f36956f097e05919cacabd935
Martin-Rojas <rojas.martin25@yahoo.com>
2025-06-16 18:46:36 -0700

    create elements form a js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import "./styles.css";
 
-alert("Martin");
+// Get parent element
+const content = document.getElementById("content");
+// Create new element
+const h1Element = document.createElement(`h1`);
+// Add content
+h1Element.innerHTML = `Restaurant`;
+// append to the parent
+content.appendChild(h1Element);
+
+// create P element
+const pElement = document.createElement("p");
+pElement.innerText = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+fugit tenetur nulla qui eos numquam dolorum laboriosam distinctio
+voluptatem`;
+content.appendChild(pElement);
+
+// Create a Button element
+const btnElement = document.createElement("button");
+btnElement.innerText = `Menu`;
+content.appendChild(btnElement);
+
+// Create an img element
+const imgElement = document.createElement(`img`);
+imgElement.setAttribute(`src`, `../image01.avif`);
+content.appendChild(imgElement);
