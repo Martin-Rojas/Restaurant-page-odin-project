7a8a973de4a81f9798a7a4155a32c3d6f5fd85e9
Martin-Rojas <rojas.martin25@yahoo.com>
2025-06-16 18:59:49 -0700

    code update home page basic design done

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 import "./styles.css";
 
-// Get parent element
-const content = document.getElementById("content");
-// Create new element
-const h1Element = document.createElement(`h1`);
-// Add content
-h1Element.innerHTML = `Restaurant`;
-// append to the parent
-content.appendChild(h1Element);
+ // Get parent element
+ const content = document.getElementById("content");
+ // Create new element
+ const h1Element = document.createElement(`h1`);
+ // Add content
+ h1Element.innerHTML = `Restaurant`;
+ // append to the parent
+ content.appendChild(h1Element);
 
-// create P element
-const pElement = document.createElement("p");
-pElement.innerText = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-fugit tenetur nulla qui eos numquam dolorum laboriosam distinctio
-voluptatem`;
-content.appendChild(pElement);
+ // create P element
+ const pElement = document.createElement("p");
+ pElement.innerText = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+   fugit tenetur nulla qui eos numquam dolorum laboriosam distinctio
+   voluptatem`;
+ content.appendChild(pElement);
 
-// Create a Button element
-const btnElement = document.createElement("button");
-btnElement.innerText = `Menu`;
-content.appendChild(btnElement);
+ // Create a Button element
+ const btnElement = document.createElement("button");
+ btnElement.innerText = `Menu`;
+ content.appendChild(btnElement);
 
-// Create an img element
-const imgElement = document.createElement(`img`);
-imgElement.setAttribute(`src`, `../image01.avif`);
-content.appendChild(imgElement);
+ // Create an img element
+ const imgElement = document.createElement(`img`);
+ imgElement.setAttribute(
+   `src`,
+   "https://hasanalii73.github.io/Restaurant-page/2906fdacc9c7007d5919.jpeg"
+ );
+ content.appendChild(imgElement);
