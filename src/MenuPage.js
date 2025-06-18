913fe1cfab8925e61f53b48615070132c1396673
Martin-Rojas <rojas.martin25@yahoo.com>
2025-06-17 19:36:00 -0700

    code update menu page done

--- src/MenuPage.js
+++ src/MenuPage.js
@@ -7,9 +7,37 @@ export default function createMenuPage() {
 
   // Create a div to content all menu items
   const menuItems = document.createElement(`div`);
+  menuItems.setAttribute(`id`, `menu-container`);
+
+  for (let items = 1; items < 10; items++) {
+    // Create a card element with menu items
+    const menuItem = document.createElement(`div`);
+    menuItem.classList.add("card");
+
+    const itemName = document.createElement(`h3`);
+    itemName.innerHTML = `Item ${items}`;
+    menuItem.appendChild(itemName);
+
+    const price = document.createElement(`p`);
+    price.innerHTML = `$ 13.50`;
+    menuItem.appendChild(price);
+
+    const itemImg = document.createElement(`img`);
+    itemImg.setAttribute(
+      `src`,
+      `https://plus.unsplash.com/premium_photo-1675252369719-dd52bc69c3df?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`
+    );
+    menuItem.appendChild(itemImg);
+
+    menuItems.appendChild(menuItem);
+
+    contentElement.appendChild(menuItems);
+  }
 
   // Create a card element with menu items
   const menuItem = document.createElement(`div`);
+  menuItem.classList.add("card");
+
   const itemName = document.createElement(`h3`);
   itemName.innerHTML = `Item 1`;
   menuItem.appendChild(itemName);
@@ -25,5 +53,7 @@ export default function createMenuPage() {
   );
   menuItem.appendChild(itemImg);
 
-  contentElement.appendChild(menuItem);
+  menuItems.appendChild(menuItem);
+
+  contentElement.appendChild(menuItems);
 }
